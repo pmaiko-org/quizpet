ae2e786e91469f5d6a11a77c2811fc81340250c2
petyamaiko <petyamaiko@gmail.com>
2026-04-02 13:03:26 +0300

    Update cache-control headers in `nuxt.config.ts` to enforce stricter caching rules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -21,7 +21,8 @@ export default defineNuxtConfig({
   routeRules: {
     "/**": {
       headers: {
-        "Cache-Control": "no-cache",
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+        "Pragma": "no-cache",
       },
     },
   },
