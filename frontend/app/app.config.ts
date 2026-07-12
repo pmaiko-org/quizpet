fcb26046905abcbc211d3e7224349ef8b195671d
petyamaiko <petyamaiko@gmail.com>
2026-07-12 20:03:39 +0300

    config

--- frontend/app/app.config.ts
+++ frontend/app/app.config.ts
@@ -1,1 +1,10 @@
-export default defineAppConfig({});
+export default defineAppConfig({
+  ui: {
+    formField: {
+      slots: {
+        root: "flex flex-col",
+        description: "text-xs/4",
+      },
+    },
+  },
+});
